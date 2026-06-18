4a808226b7f96f97e1237b6a8f0a8bf2a8adb22d
CouCave <phankhang1945@gmail.com>
2026-06-18 22:59:57 +0700

    admin: CRUD sản phẩm, danh mục; load user, khu vực

--- Backend/FastGuy-FastFoodSite/src/main/java/dao/DeliveryZoneDAO.java
+++ Backend/FastGuy-FastFoodSite/src/main/java/dao/DeliveryZoneDAO.java
@@ -20,8 +20,52 @@ public class DeliveryZoneDAO {
         EntityManager em = DatabaseUtil.getEntityManager();
         try {
             return em.createQuery(
-                    "SELECT z FROM DeliveryZone z WHERE z.isActive = true", DeliveryZone.class)
+                    "SELECT z FROM DeliveryZone z WHERE z.isActive = true ORDER BY z.zoneId",
+                    DeliveryZone.class)
                     .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<DeliveryZone> findAll() {
+        EntityManager em = DatabaseUtil.getEntityManager();
+        try {
+            return em.createQuery("SELECT z FROM DeliveryZone z ORDER BY z.zoneId", DeliveryZone.class)
+                    .getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void save(DeliveryZone zone) {
+        EntityManager em = DatabaseUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            if (zone.getZoneId() == 0) {
+                em.persist(zone);
+            } else {
+                em.merge(zone);
+            }
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+    }
+
+    public void delete(int id) {
+        EntityManager em = DatabaseUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            DeliveryZone z = em.find(DeliveryZone.class, id);
+            if (z != null) em.remove(z);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
         } finally {
             em.close();
         }
